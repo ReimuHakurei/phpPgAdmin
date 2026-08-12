16a3e002542dcaf8a714334e35def2c57ed1c35d
ReimuHakurei <alex@alexingram.net>
2026-08-12 23:51:26 +0000

    Bump version and update HISTORY.

--- libraries/lib.inc.php
+++ libraries/lib.inc.php
@@ -16,7 +16,7 @@
 	$appName = 'phpPgAdmin';
 
 	// Application version
-	$appVersion = '7.14.7-mod';
+	$appVersion = '7.14.8-mod';
 
 	// PostgreSQL and PHP minimum version
 	global $postgresqlMinVer;
